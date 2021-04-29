a6ffda6d739585a60da9c1ef15b1d32e09fe08c7
Hrishabh Kumar <hrishabh0007@gmail.com>
2021-04-29 09:41:50 +0530

    Initial Commit

---
+++ restful-web-services/src/main/java/com/spring/rest/restfulwebservices/user/UserDAOService.java
@@ -0,0 +1,40 @@
+package com.spring.rest.restfulwebservices.user;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDAOService {
+	
+	private static List<User> userList = new ArrayList<>();
+	private static int userCount = 3;
+	
+	static {
+		userList.add(new User(1, "Jack", new Date()));
+		userList.add(new User(2, "Jill", new Date()));
+		userList.add(new User(3, "John", new Date()));
+	}
+	
+	public List<User> findAllUser(){
+		return userList;
+	}
+	
+	public User save(User user) {
+		
+		if(user.getId() == null) {
+			user.setId(++userCount);
+		}
+		userList.add(user);
+		return user;
+	}
+	
+	public User findOne(int id) {
+		for(User user: userList) {
+			if(user.getId() == id)
+				return user;
+		}
+		return null;
+	}
+}
---
+++ restful-web-services/src/main/java/com/spring/rest/restfulwebservices/user/UserResource.java
@@ -0,0 +1,40 @@
+package com.spring.rest.restfulwebservices.user;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserResource {
+	
+	@Autowired
+	private UserDAOService service;
+	
+	public void setService(UserDAOService service) {
+		this.service = service;
+	}
+
+	//retrieveAllUsers
+	@GetMapping("/users")
+	public List<User> retrieveAllUsers(){
+		return service.findAllUser();
+	}
+	
+	
+	//retrieveUser by Id ()
+	@GetMapping("user/{id}")
+	public User getUserById(@PathVariable int id) {
+		List<User> userList = service.findAllUser();
+		
+		for(User usr : userList) {
+			if(usr.getId() == id)
+				return usr;
+		}
+		return null;
+	}
+	
+
+}
